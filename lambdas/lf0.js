900a75ccb7a2a17878ba25bcac54ebd8bbc94877
Dhanush Raya <rayadhanush@gmail.com>
2024-10-18 15:53:58 -0400

    refactored lf0

--- lambdas/lf0.js
+++ lambdas/lf0.js
@@ -2,42 +2,36 @@ import {
   LexRuntimeV2Client,
   RecognizeTextCommand,
 } from "@aws-sdk/client-lex-runtime-v2";
-// import { v4 as uuidv4 } from "uuid";
 import { randomUUID } from "crypto";
+
 const lex = new LexRuntimeV2Client();
 
 // Helper to create an error response
-function createError(code, message) {
-  return {
-    code: code,
-    message: message,
-  };
-}
+const createError = (code, message) => ({
+  code: code,
+  message: message,
+});
 
 // Helper to create an unstructured message
-function createUnstructuredMessage(id, text, timestamp) {
-  return {
-    id: id,
-    text: text,
-    timestamp: timestamp,
-  };
-}
+const createUnstructuredMessage = (id, text, timestamp) => ({
+  id: id,
+  text: text,
+  timestamp: timestamp,
+});
 
 // Helper to create a message
-function createMessage(uMessage, type = "unstructured") {
-  return {
-    type: type,
-    unstructured: uMessage,
-  };
-}
+const createMessage = (uMessage, type = "unstructured") => ({
+  type: type,
+  unstructured: uMessage,
+});
 
 // Helper to create a bot response
-function createBotResponse(messages) {
-  return { messages: messages };
-}
+const createBotResponse = (messages) => ({
+  messages: messages,
+});
 
 // Helper to create simple messages
-function createSimpleMessage(msgs) {
+const createSimpleMessage = (msgs) => {
   const parsedMessages = msgs.map((msg) => {
     const timestamp = Date.now();
     const unstructured = createUnstructuredMessage(
@@ -48,22 +42,21 @@ function createSimpleMessage(msgs) {
     return createMessage(unstructured);
   });
   return createBotResponse(parsedMessages);
-}
+};
 
 // Helper to parse response from Lex
-function parseResponse(response) {
+const parseResponse = (response) => {
   const httpStatus = response["$metadata"].httpStatusCode;
   if (response.messages) {
     return response.messages.map((msg) => msg.content);
   } else {
     return ["What can I help you with?"];
   }
-}
+};
 
 // Function to send a message to Lex V2
-async function postToBot(event) {
+const postToBot = async (event) => {
   const msg = event.messages[0].unstructured.text;
-  console.log(`Parsed message: ${msg}`);
 
   const params = {
     botId: "GK7NIBALMY",
@@ -74,26 +67,25 @@ async function postToBot(event) {
   };
 
   try {
-    // const response = await lex.recognizeText(params).promise();
     const command = new RecognizeTextCommand(params);
+
     const response = await lex.send(command);
     console.log(`Received response from Lex: ${JSON.stringify(response)}`);
+
     return parseResponse(response);
   } catch (error) {
     console.error("Error communicating with Lex:", error);
     throw error;
   }
-}
+};
 
 // Lambda handler function
 export const handler = async (event) => {
-  console.log("lf0");
-  console.log(`Event: ${JSON.stringify(event)}`);
+  console.log("lf0 triggered with payload:", JSON.stringify(event));
 
   try {
     const responseMessage = await postToBot(event);
     const response = createSimpleMessage(responseMessage);
-    console.log("returning response");
     return response;
   } catch (error) {
     console.log(error);
